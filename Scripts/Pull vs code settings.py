761b53ba56dfbbb72028744fc9c79f7a3f5fb101
Axlefublr <Axlefublr@gmail.com>
2022-03-26 14:36:42 +0700

    A better way to save settings: no git init in the actual settings and instead a hotkey to running a python file that copies the actually needed settings into the backup folder of this repository. Hotkey for an inputbox to type in the name to open the actual settings' jsons

---
+++ Scripts/Pull vs code settings.py
@@ -0,0 +1,12 @@
+# copyfile() copies contents of a file
+# copy() + permission mode + destination can be a directory + L + ratio
+# copy2() copy() copies metadata (file's creation and modification times) 
+import shutil
+
+shutil.copyfile("C:\\Users\\serge\\AppData\\Roaming\\Code\\User\\snippets\\ahk.json", "Backups\\ahk.jsonc") #source, destination (args the same for each copy func)
+shutil.copyfile("C:\\Users\\serge\\AppData\\Roaming\\Code\\User\\snippets\\jsonc.json", "Backups\\jsonc.jsonc") #source, destination (args the same for each copy func)
+shutil.copyfile("C:\\Users\\serge\\AppData\\Roaming\\Code\\User\\snippets\\plaintext.json", "Backups\\plaintext.jsonc") #source, destination (args the same for each copy func)
+shutil.copyfile("C:\\Users\\serge\\AppData\\Roaming\\Code\\User\\keybindings.json", "Backups\\keybindings.json") #source, destination (args the same for each copy func)
+shutil.copyfile("C:\\Users\\serge\\AppData\\Roaming\\Code\\User\\settings.json", "Backups\\settings.json") #source, destination (args the same for each copy func)
+shutil.copyfile("C:\\Users\\serge\\AppData\\Roaming\\Code\\User\\tasks.json", "Backups\\tasks.json") #source, destination (args the same for each copy func)
+shutil.copyfile("C:\\Users\\serge\\.vscode\\extensions\\mark-wiemer.vscode-autohotkey-plus-plus-2.8.1\\syntaxes\\ahk.tmLanguage.json", "Backups\\ahk.tmLanguage.json") #source, destination (args the same for each copy func)

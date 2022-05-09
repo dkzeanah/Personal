948eb0c706394650112f799ea4651de3d4fb261d
Axlefublr <Axlefublr@gmail.com>
2022-05-09 14:58:04 +0700

    notepad in runner; up down with space in docs; settings sync; intellisense stopped working

--- Scripts/Pull vs code settings.py
+++ Scripts/Pull vs code settings.py
@@ -1,10 +1,3 @@
 import shutil
 
-shutil.copyfile("C:\\Users\\serge\\AppData\\Roaming\\Code\\User\\snippets\\ahk.json", "Backups\\ahk.jsonc") #source, destination (args the same for each copy func)
-shutil.copyfile("C:\\Users\\serge\\AppData\\Roaming\\Code\\User\\snippets\\jsonc.json", "Backups\\jsonc.jsonc") #source, destination (args the same for each copy func)
-shutil.copyfile("C:\\Users\\serge\\AppData\\Roaming\\Code\\User\\snippets\\plaintext.json", "Backups\\plaintext.jsonc") #source, destination (args the same for each copy func)
-shutil.copyfile("C:\\Users\\serge\\AppData\\Roaming\\Code\\User\\keybindings.json", "Backups\\keybindings.json") #source, destination (args the same for each copy func)
-shutil.copyfile("C:\\Users\\serge\\AppData\\Roaming\\Code\\User\\settings.json", "Backups\\settings.json") #source, destination (args the same for each copy func)
-shutil.copyfile("C:\\Users\\serge\\AppData\\Roaming\\Code\\User\\tasks.json", "Backups\\tasks.json") #source, destination (args the same for each copy func)
 shutil.copyfile("C:\\Users\\serge\\.vscode\\extensions\\mark-wiemer.vscode-autohotkey-plus-plus-2.8.1\\syntaxes\\ahk.tmLanguage.json", "Backups\\ahk.tmLanguage.json") #source, destination (args the same for each copy func)
-shutil.copyfile("C:\\Users\\serge\\.vscode\\extensions\\mark-wiemer.vscode-autohotkey-plus-plus-2.8.1\\package.json", "Backups\\package.json") #source, destination (args the same for each copy func)

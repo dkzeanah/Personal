d286a1b8058d3173d88f7d9a76c8cbd542f432b5
Axlefublr <Axlefublr@gmail.com>
2022-05-14 19:42:53 +0700

    Better organization of files I intend to have for others / myself; Libload is now separate: Lib and Load; No more python just to give it a try, ahk is better in this situation

--- Scripts/Pull vs code settings.py
+++
@@ -1,3 +0,0 @@
-import shutil
-
-shutil.copyfile("C:\\Users\\serge\\.vscode\\extensions\\mark-wiemer.vscode-autohotkey-plus-plus-2.8.1\\syntaxes\\ahk.tmLanguage.json", "Backups\\ahk.tmLanguage.json") #source, destination (args the same for each copy func)
